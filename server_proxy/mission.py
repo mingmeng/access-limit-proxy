af1b75472c9ae5342fd1211c2621724a32f882e2
mingmeng <i@yangruixin.com>
2019-01-11 19:27:44 +0800

    update comment

--- server_proxy/mission.py
+++ server_proxy/mission.py
@@ -44,7 +44,7 @@ class ProxyAccess(app_manager.RyuApp):
         self.mac_to_port = {}
         self.ip_to_mac = {}
         self.dps = []
-	self.limit={}
+        self.limit={}       # 加上流表限制属性 用于存储
         self.web_server_dp = None
         hub.spawn(self._redirect)
 
@@ -55,11 +55,11 @@ class ProxyAccess(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         self.dps.append(datapath)
-        # add goto table 1 flow entry on table 0
+        # 将表0的流表项加入表1
         match = parser.OFPMatch()
         inst = [parser.OFPInstructionGotoTable(table_id=1)]
         self.add_flow(datapath, 0, match, table_id=0, inst=inst)
-        # install table-miss flow entry on table 1
+        # 给表1加上流表默认项
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions, table_id=1)
@@ -79,7 +79,7 @@ class ProxyAccess(app_manager.RyuApp):
                                     hard_timeout=hard_timeout, match=match, instructions=inst)
         datapath.send_msg(mod)
 
-    # install redirect flows
+    # 加入重定向流表项
     def _redirect(self):
         hub.sleep(3)
         self.ip_to_mac.setdefault(host_proxy)
@@ -87,6 +87,7 @@ class ProxyAccess(app_manager.RyuApp):
         self.ip_to_mac.setdefault(web_proxy)
 
         # detect mac of host_proxy web_server and web_proxy
+        # 检测web服务器的mac地址和代理服务器的地址
         flag = False
         while not flag:
             flag = True
@@ -102,7 +103,7 @@ class ProxyAccess(app_manager.RyuApp):
 
         ofproto = ofproto_v1_3
         parser = ofproto_v1_3_parser
-        # install flow-entry prevent  common host from  visiting web_proxy
+        # 把非代理用户限制访问的流表项加入
         match = parser.OFPMatch(
             eth_type=ether.ETH_TYPE_IP, ipv4_dst=web_proxy,
             ip_proto=inet.IPPROTO_TCP, tcp_dst=80)
@@ -113,7 +114,7 @@ class ProxyAccess(app_manager.RyuApp):
         on switch %s" % (web_proxy, self.web_server_dp.id))
 
         hub.sleep(20)
-        # install rederict flow-enty that flow src_ip is host_proxy
+        # 将来自代理用户的转发重定向
         match = parser.OFPMatch(
             eth_type=ether.ETH_TYPE_IP, ipv4_src=host_proxy, ipv4_dst=web_server,
             ip_proto=inet.IPPROTO_TCP, tcp_dst=80)
@@ -125,7 +126,6 @@ class ProxyAccess(app_manager.RyuApp):
                 parser.OFPInstructionGotoTable(table_id=1)]
         self.add_flow(self.web_server_dp, 10, match, table_id=0, inst=inst)
 
-        # install rederict flow-enty that flow src_ip is web_proxy
         match = parser.OFPMatch(
             eth_type=ether.ETH_TYPE_IP, ipv4_src=web_proxy, ipv4_dst=host_proxy,
             ip_proto=inet.IPPROTO_TCP, tcp_src=80)
@@ -137,8 +137,7 @@ class ProxyAccess(app_manager.RyuApp):
                 parser.OFPInstructionGotoTable(table_id=1)]
         self.add_flow(self.web_server_dp, 10, match, table_id=0, inst=inst)
 
-    # send detect_arp request to acquire host_proxy web_server
-    # and web_proxy mac
+    
     def detect_mac(self, ip_addr):
         pkt = packet.Packet()
         eth_r = ethernet.ethernet(ethertype=ether.ETH_TYPE_ARP,
@@ -161,8 +160,6 @@ class ProxyAccess(app_manager.RyuApp):
             datapath.send_msg(arp_request)
         print("send detect_arp request:i am %s,who is %s" % (detect_ip, ip_addr))
 
-    # recieve detect_arp reply to acquire host_proxy web_server
-    # and web_proxy mac and datapath directly connecting the web_server
     def arp_reply_handler(self, msg, datapath, eth, arp):
         if (arp.opcode == 2) and (eth.dst == detect_mac):
             for ip_addr in self.ip_to_mac.keys():
@@ -175,8 +172,6 @@ class ProxyAccess(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        # If you hit this you might want to increase
-        # the "miss_send_length" of your switch
         if ev.msg.msg_len < ev.msg.total_len:
             self.logger.debug("packet truncated: only %s of %s bytes",
                               ev.msg.msg_len, ev.msg.total_len)
@@ -191,13 +186,11 @@ class ProxyAccess(app_manager.RyuApp):
         dst = eth.dst
         src = eth.src
 
-        # handle arp to learn mac and datapath directly
         if(eth.ethertype == ether.ETH_TYPE_ARP):
             arp_r = pkt.get_protocol(arp.arp)
             if (arp_r.src_ip == detect_ip) or (arp_r.dst_ip == detect_ip):
                 self.arp_reply_handler(
                     msg, datapath, eth, pkt.get_protocol(arp.arp))
-                # return to prenvent from detect ip flood
                 return
 
         dpid = datapath.id
@@ -205,7 +198,6 @@ class ProxyAccess(app_manager.RyuApp):
 
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-        # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
 
         if dst in self.mac_to_port[dpid]:
@@ -215,11 +207,8 @@ class ProxyAccess(app_manager.RyuApp):
 
         actions = [parser.OFPActionOutput(out_port)]
 
-        # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
             match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            # verify if we have a valid buffer_id, if yes avoid to send both
-            # flow_mod & packet_out
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
                 self.add_flow(datapath, 1, match, actions, table_id=1,
                               idle_timeout=10, hard_timeout=10, buffer_id=msg.buffer_id)
@@ -239,17 +228,15 @@ class ProxyAccess(app_manager.RyuApp):
                 pkt_tcp = pkt.get_protocol(tcp.tcp)
                 if 80 == pkt_tcp.dst_port and "10.0.0.2"==pkt_ipv4.dst and "10.0.0.1"==pkt_ipv4.src:
                     print(" tcp dst_prot~~~~~~~~~~~~~%d" %pkt_tcp.dst_port)
+                    # 如果是第一次访问 将机器设为已经被限制
                     if (pkt_ipv4.src, pkt_ipv4.dst, pkt_tcp.dst_port) not in self.limit.keys():
                         self.limit[
                             (pkt_ipv4.src, pkt_ipv4.dst, pkt_tcp.dst_port)] = True
                     if self.limit[(pkt_ipv4.src, pkt_ipv4.dst, pkt_tcp.dst_port)]:
-                        # set FLASE , don`t "access deny" next time.
-                        #self.limit[
-                        #    (pkt_ipv4.src, pkt_ipv4.dst, pkt_tcp.dst_port)] = False
-                        # can` have value in monitor!!!
+                        # 如果在限制列表里面 就将该机器ban掉
                         hub.spawn(self._monitor, datapath, pkt_ipv4, pkt_tcp)
 
-    # function:limit 60 s
+    # 将机器ban掉60秒
     def _monitor(self, datapath, pkt_ipv4, pkt_tcp):
         hub.sleep(10)
         ipv4_src = pkt_ipv4.src
